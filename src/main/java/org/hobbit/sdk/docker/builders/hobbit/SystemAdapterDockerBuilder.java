1f9c14a75e1d14ae3ccedfa8c2ebce5adcb25aff
Denis Kuchelev <denkv@users.noreply.github.com>
2019-02-15 19:25:12 +0100

    Add benchmark and system containers only to hobbit network

--- src/main/java/org/hobbit/sdk/docker/builders/hobbit/SystemAdapterDockerBuilder.java
+++ src/main/java/org/hobbit/sdk/docker/builders/hobbit/SystemAdapterDockerBuilder.java
@@ -4,7 +4,7 @@ import org.hobbit.sdk.docker.builders.AbstractDockersBuilder;
 import org.hobbit.sdk.docker.builders.BothTypesDockersBuilder;
 
 import static org.hobbit.core.Constants.*;
-import static org.hobbit.sdk.Constants.HOBBIT_NETWORKS;
+import static org.hobbit.sdk.Constants.*;
 
 
 /**
@@ -34,7 +34,7 @@ public class SystemAdapterDockerBuilder extends BothTypesDockersBuilder {
     public void addEnvVars(AbstractDockersBuilder ret) {
         ret.addEnvironmentVariable(RABBIT_MQ_HOST_NAME_KEY, (String)System.getenv().get(RABBIT_MQ_HOST_NAME_KEY));
         ret.addEnvironmentVariable(HOBBIT_SESSION_ID_KEY, (String)System.getenv().get(HOBBIT_SESSION_ID_KEY));
-        ret.addNetworks(HOBBIT_NETWORKS);
+        ret.addNetworks(new String[]{ HOBBIT_NETWORK_NAME });
 
         ret.addEnvironmentVariable(SYSTEM_PARAMETERS_MODEL_KEY, (String)System.getenv().get(SYSTEM_PARAMETERS_MODEL_KEY));
         //ret.addEnvironmentVariable(CONTAINER_NAME_KEY, ret.getContainerName());
